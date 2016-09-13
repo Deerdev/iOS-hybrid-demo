854aeb881bf0d000cdef6f0191a860fbabc9ac8c
daoquan.lu <daoquan.lu@feixun.com.cn>
2016-09-13 13:50:33 +0800

    Initial Commit

---
+++ HybridTestDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HybridTestDemo
+//
+//  Created by daoquan on 16/9/13.
+//  Copyright © 2016年 daoquan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
